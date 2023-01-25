288bc688099c7bde21216132e8b2f9c35cbae403
jew-de <wendland.julius@gmail.com>
2023-01-25 16:31:08 +0100

    created basic landing page for testing purposes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <></>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <main className='flex max-w-4xl sm:w-full flex-wrap items-center justify-around'>
+        <p className='w-96 p-10'>Dies ist ein Test!</p>
+        <p className='w-96 p-10'>Hier entsteht bald:</p>
+        <p className='w-96 p-10'>Eine Landing Page mit</p>
+        <p className='w-96 p-10'>Einem Dashboard</p>
+      </main>
+    </div>
   )
 }
 
