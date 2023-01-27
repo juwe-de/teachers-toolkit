d025ffeaeeffaec7674a451dee27b044a8d617d8
jew-de <wendland.julius@gmailcom>
2023-01-27 10:30:38 +0100

    created toggle menu and implemented profile picture

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link"
+import Image from "next/image"
 
 import { useState } from "react"
 import { AiOutlineSearch } from "react-icons/ai"
+
+import Menu from "./Menu"
 
 const Header = () => {
     const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
     const burgerButtonLine = "h-1 w-6 my-1 rounded-full transition ease transform duration-300"
 
     return (
-        <div className="w-full h-12 mb-2 bg-blue-500 top-0 flex flex-row items-center px-4 space-x-8 justify-between shadow-lg relative">
+        <div className="w-full h-12 mb-2 bg-blue-500 top-0 flex flex-row items-center px-4 justify-between shadow-lg relative">
             {/* Burger menu button (only shown when on small devices) */}
             <button 
                 className="flex md:hidden flex-col h-12 w-12 rounded justify-center items-center group"
                 onClick={() => setBurgerMenuOpen(!burgerMenuOpen)}
             >
+                {/* Three lines forming a hamburger button */}
+                {/* When hovered they form an X */}
                 <div 
                     className={
                         `${burgerButtonLine} ${burgerMenuOpen ? "rotate-45 translate-y-3 bg-slate-200 group-hover:bg-slate-50" : "bg-slate-200 group-hover:bg-slate-50"}`
@@ -41,7 +46,7 @@ const Header = () => {
             <div className="md:hidden opacity-0 h-12 w-12" />
 
             {/* Serchbar */}
-            <div className="hidden md:flex flex-row flex-1 bg-slate-50 h-4/5 max-w-5xl rounded-md shadow-md items-center space-x-2">
+            <div className="hidden md:flex flex-row flex-1 bg-slate-50 h-4/5 max-w-5xl rounded-md shadow-md items-center space-x-2 ml-2 xl:m-0">
                 <AiOutlineSearch className="h-7 w-7 text-stone-900 ml-1 bg-slate-200 p-1 rounded-md"/>
                 <input className="bg-transparent focus:outline-none text-stone-800 w-full" placeholder="Suche nach irgendetwas..." />
             </div>
@@ -49,8 +54,16 @@ const Header = () => {
             {/* Account details (COMING SOON) */}
             <div className="hidden md:flex w-60 xl:w-96 flex-row space-x-1 justify-end text-lg text-slate-50">
                 <p className="font-semibold">Heya,</p>
-                <p className="underline">Chrstopher</p>
+                <p className="underline">Christopher</p>
+                <Image src="/profile.jpg" alt={"Profilbild"} width={35} height={35} className="rounded-full border-2 border-slate-50" />
             </div>
+
+            {
+                // Only show menu when button is active
+                burgerMenuOpen && (
+                    <Menu  />
+                )
+            }
         </div>
     )
 }
